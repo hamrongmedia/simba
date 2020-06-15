f47a539c1aa99a1e75a9f76cc96d040d04dcc81f
tranhoang-c5 <tranhoang750@gmail.com>
2020-06-15 19:12:28 +0700

    fix conflic

--- database/migrations/2020_06_12_031937_hrm_create_tables_admin.php
+++ database/migrations/2020_06_12_031937_hrm_create_tables_admin.php
@@ -17,12 +17,12 @@ class HrmCreateTablesAdmin extends Migration
             $table->bigIncrements('id');
             $table->string('slug')->unique();
             $table->string('title');
-            $table->string('image', 200)->nullable();
+            $table->string('image', 200)->nullable(); // Nếu bài post có 2 image trở lên se lỗi. mà post thì k cần image cx dc
             $table->string('description', 300)->nullable();
             $table->text('content')->nullable();
-            $table->integer('cat_id')->nullable();
+            $table->integer('cat_id')->nullable(); // 1 post có thể có nhiều category
             $table->integer('created_by')->nullable();
-            $table->boolean('status');
+            $table->boolean('status'); // nếu có 3 trạng thái trở lên thì lỗi
             $table->text('tags')->nullable();
             $table->string('meta_des')->nullable();
             $table->string('meta_key')->nullable();
@@ -36,7 +36,7 @@ class HrmCreateTablesAdmin extends Migration
             $table->string('name');
             $table->integer('cat_id')->nullable();
             $table->string('description', 300)->nullable();
-            $table->boolean('status');
+            $table->boolean('status'); // nếu có 3 trạng thái trở lên thì lỗi
             $table->string('meta_des')->nullable();
             $table->string('meta_key')->nullable();
             $table->string('meta_title')->nullable();
