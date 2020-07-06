124ae11f63c63fd297a2cfccaa422ce018abcf69
tranhoang-c5 <tranhoang750@gmail.com>
2020-07-06 23:05:40 +0700

    sửa hiển thị đo size

--- public/js/customer.js
+++ public/js/customer.js
@@ -1,42 +1,37 @@
-jQuery(document).ready(function($) {
-    $(document).ready(function(){
+jQuery(document).ready(function ($) {
+    $(document).ready(function () {
         var stickyTop = $("#sticky-wrapper").offset().top;
-        jQuery(window).scroll(function(){
-            if (jQuery(window).scrollTop() > stickyTop){
-                jQuery('.sticky-wrapper').addClass('header-sticky');
-            }
-            else{
-                jQuery('.sticky-wrapper').removeClass('header-sticky');
+        jQuery(window).scroll(function () {
+            if (jQuery(window).scrollTop() > stickyTop) {
+                jQuery(".sticky-wrapper").addClass("header-sticky");
+            } else {
+                jQuery(".sticky-wrapper").removeClass("header-sticky");
             }
         });
     });
-    $(".btn-click-cart").click( function (){
+    $(".btn-click-cart").click(function () {
         $("#site-cart").addClass("active");
     });
-    $("#site-close-handle").click( function (){
+    $("#site-close-handle").click(function () {
         $("#site-cart").removeClass("active");
     });
     $(".btn-search-mb").click(function () {
         $(this).toggleClass("open");
-        $(".wp-box-search-mb").slideToggle( "fast", function() {
-        });
+        $(".wp-box-search-mb").slideToggle("fast", function () {});
     });
 
+    jQuery(".mobile-main-menu .menu-item-has-children > a").after(
+        '<i class="fa fa-plus fa1" aria-hidden="true"></i>'
+    );
 
-
-    jQuery('.mobile-main-menu .menu-item-has-children > a').after('<i class="fa fa-plus fa1" aria-hidden="true"></i>');
-
-    jQuery('.mobile-main-menu .menu-item-has-children .fa1').click( function () {
-        jQuery(this).closest('li').children('.sub-menu').toggle(600);
-        jQuery(this).toggleClass('fa1');
+    jQuery(".mobile-main-menu .menu-item-has-children .fa1").click(function () {
+        jQuery(this).closest("li").children(".sub-menu").toggle(600);
+        jQuery(this).toggleClass("fa1");
     });
 
-
-
-
-    $('.wp-menu-mobile').click( function(){
-         $('.mobile-main-menu').slideToggle(200);
-         $('.wp-main-header').toggleClass('bg-white');
+    $(".wp-menu-mobile").click(function () {
+        $(".mobile-main-menu").slideToggle(200);
+        $(".wp-main-header").toggleClass("bg-white");
     });
 
     // $('.ng-has-child1 .ul-has-child1 .ng-has-child2 a .fa2').on('click', function(e){
@@ -81,114 +76,126 @@ jQuery(document).ready(function($) {
     //             $('#total-view-cart').html(json.total);
     //             $('#qtotalitems').html(json.item);
     //             $("#site-cart").addClass("active");
-                
+
     //             //Gtag nút mua ngay
     //             gtag_report_conversion(window.location.href);
     //         }
-
 
     //     });
     // });
 
     $(".btn-click-dosize").click(function () {
-        var value_chieucao = $('#value_chieucao').val();
-        var value_cannang = $('#value_cannang').val();
-        var value_vong1 = $('#value_vong1').val();
+        var value_chieucao = $("#value_chieucao").val();
+        var value_cannang = $("#value_cannang").val();
+        var value_vong1 = $("#value_vong1").val();
         test_size(value_chieucao, value_cannang, value_vong1);
         return false;
     });
-    // function test_size(value_chieucao, value_cannang, value_vong1) {
-    //     $.post('', {
-    //         value_chieucao: value_chieucao,
-    //         value_cannang: value_cannang,
-    //         value_vong1: value_vong1
-    //     }, function (data) {
-    //         var json = JSON.parse(data);
-    //         $('#form-view-2').html(json.html);
-    //         $(".wp-list-form-dosize").find(".form-1").addClass("hidden");
-    //         $(".wp-list-form-dosize").find(".form-2").addClass("active");
-    //     });
-    // }
-    $('input[name="participants"]').rangeslider({
-        polyfill: false, onInit: function () {
-            $handle = $('.rangeslider-group1 .rangeslider__handle');
+
+    function test_size(value_chieucao, value_cannang, value_vong1) {
+        //var json = JSON.parse(data);
+        //$("#form-view-2").html(json.html);
+        $(".wp-list-form-dosize").find(".form-1").addClass("hidden");
+        $(".wp-list-form-dosize").find(".form-2").addClass("active");
+    }
+
+    $('input[name="participants"]')
+        .rangeslider({
+            polyfill: false,
+            onInit: function () {
+                $handle = $(".rangeslider-group1 .rangeslider__handle");
+                updateHandle($handle[0], this.value);
+                $("#value_chieucao").val(this.value);
+            },
+        })
+        .on("input", function () {
             updateHandle($handle[0], this.value);
-            $('#value_chieucao').val(this.value);
-        }
-    }).on('input', function () {
-        updateHandle($handle[0], this.value);
-        $('#value_chieucao').val(this.value);
-    });
-    $('input[name="participants2"]').rangeslider({
-        polyfill: false, onInit: function () {
-            $handle2 = $('.rangeslider-group2 .rangeslider__handle');
+            $("#value_chieucao").val(this.value);
+        });
+    $('input[name="participants2"]')
+        .rangeslider({
+            polyfill: false,
+            onInit: function () {
+                $handle2 = $(".rangeslider-group2 .rangeslider__handle");
+                updateHandle($handle2[0], this.value);
+                $("#value_cannang").val(this.value);
+            },
+        })
+        .on("input", function () {
             updateHandle($handle2[0], this.value);
-            $('#value_cannang').val(this.value);
-        }
-    }).on('input', function () {
-        updateHandle($handle2[0], this.value);
-        $('#value_cannang').val(this.value);
-    });
-    $('input[name="participants3"]').rangeslider({
-        polyfill: false, onInit: function () {
-            $handle3 = $('.rangeslider-group3 .rangeslider__handle');
+            $("#value_cannang").val(this.value);
+        });
+    $('input[name="participants3"]')
+        .rangeslider({
+            polyfill: false,
+            onInit: function () {
+                $handle3 = $(".rangeslider-group3 .rangeslider__handle");
+                updateHandle($handle3[0], this.value);
+                $("#value_vong1").val(this.value);
+            },
+        })
+        .on("input", function () {
             updateHandle($handle3[0], this.value);
-            $('#value_vong1').val(this.value);
-        }
-    }).on('input', function () {
-        updateHandle($handle3[0], this.value);
-        $('#value_vong1').val(this.value);
-    });
+            $("#value_vong1").val(this.value);
+        });
     function updateHandle(el, val) {
         el.textContent = val;
     }
 
-
-    if($(window).width() < 768) {
+    if ($(window).width() < 768) {
         $(".wp-ft-main").click(function () {
             $(this).toggleClass("open");
-            $(this).find(".list-ft-main").slideToggle("slow", function () {
-            });
+            $(this)
+                .find(".list-ft-main")
+                .slideToggle("slow", function () {});
         });
     }
 
-
-    $(".btn-click-show-top").click( function() {
+    $(".btn-click-show-top").click(function () {
         $(this).toggleClass("active");
-        $(this).parent().parent().find(".wp-bottom-text-sp-22").slideToggle( "slow", function() {
-        });
+        $(this)
+            .parent()
+            .parent()
+            .find(".wp-bottom-text-sp-22")
+            .slideToggle("slow", function () {});
         $(this).parent().parent().toggleClass("active");
-        $(this).parent().parent().parent().parent().parent().parent().parent().toggleClass("active");
+        $(this)
+            .parent()
+            .parent()
+            .parent()
+            .parent()
+            .parent()
+            .parent()
+            .parent()
+            .toggleClass("active");
     });
 
-    $(".btn-chinhsua-lai").click( function() {
+    $(".btn-chinhsua-lai").click(function () {
         $(".wp-list-form-dosize").find(".form-1").removeClass("hidden");
         $(".wp-list-form-dosize").find(".form-2").removeClass("active");
     });
-     $(".btn-click-dosize").click( function() {
+    $(".btn-click-dosize").click(function () {
         $(".wp-list-form-dosize").find(".form-1").addClass("hidden");
         $(".wp-list-form-dosize").find(".form-2").addClass("active");
     });
-    $(".closedong").click( function() { 
+    $(".closedong").click(function () {
         $(".wp-list-form-dosize").find(".form-1").removeClass("hidden");
         $(".wp-list-form-dosize").find(".form-2").removeClass("active");
     });
 
-    $(".btn-click-boloc").click( function (){
-        $('body, html').addClass("active");
+    $(".btn-click-boloc").click(function () {
+        $("body, html").addClass("active");
         $(this).parent().addClass("open");
         $(this).parent().find(".wp-bo-loc-1").addClass("open");
     });
 
-    $(".close-fil").click( function (){
-        $('body, html').removeClass("active");
+    $(".close-fil").click(function () {
+        $("body, html").removeClass("active");
 
         $(this).parent().removeClass("open");
 
         $(this).parent().find(".wp-bo-loc-1").removeClass("open");
-
     });
-    
 
     $(".regular").slick({
         dots: false,
@@ -198,78 +205,105 @@ jQuery(document).ready(function($) {
         vertical: true,
     });
 
-    $('.slide-uudai').owlCarousel({
-        loop:true,
-        margin:1,
-        dots:false,
-        nav:false,
-        autoplay:true,
-        autoplayTimeout:5000,
-        autoplaySpeed:1200,
-        smartSpeed:1200,
-        responsive:{
-            0:{
-
-                items:2
-
+    $(".slide-uudai").owlCarousel({
+        loop: true,
+        margin: 1,
+        dots: false,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplaySpeed: 1200,
+        smartSpeed: 1200,
+        responsive: {
+            0: {
+                items: 2,
             },
-            320:{
-
-                items:2
-
+            320: {
+                items: 2,
             },
-            767:{
-
-                items:3
-
+            767: {
+                items: 3,
             },
-            1000:{
-
-                items:3
-
-            }
-        }
+            1000: {
+                items: 3,
+            },
+        },
     });
 
     // js sản phẩm
-    $(".list-color-sp li").click( function() {
-        $(this).parents(".list-color-sp").find(".item-color").removeClass('active');
-        $(this).addClass('active');
+    $(".list-color-sp li").click(function () {
+        $(this)
+            .parents(".list-color-sp")
+            .find(".item-color")
+            .removeClass("active");
+        $(this).addClass("active");
     });
 
-
-    $(".list-color-sp li").click( function() {
-        $(this).parents(".wp-item-sp").find(".wp-item-sp-main").removeClass('active');
+    $(".list-color-sp li").click(function () {
+        $(this)
+            .parents(".wp-item-sp")
+            .find(".wp-item-sp-main")
+            .removeClass("active");
     });
-    $(".list-color-sp li:nth-child(1)").click( function() {
-        $(this).parents(".wp-item-sp").find(".wp-item-sp-main:nth-child(1)").addClass('active');
+    $(".list-color-sp li:nth-child(1)").click(function () {
+        $(this)
+            .parents(".wp-item-sp")
+            .find(".wp-item-sp-main:nth-child(1)")
+            .addClass("active");
     });
-    $(".list-color-sp li:nth-child(2)").click( function() {
-        $(this).parents(".wp-item-sp").find(".wp-item-sp-main:nth-child(2)").addClass('active');
+    $(".list-color-sp li:nth-child(2)").click(function () {
+        $(this)
+            .parents(".wp-item-sp")
+            .find(".wp-item-sp-main:nth-child(2)")
+            .addClass("active");
     });
-    $(".list-color-sp li:nth-child(3)").click( function() {
-        $(this).parents(".wp-item-sp").find(".wp-item-sp-main:nth-child(3)").addClass('active');
+    $(".list-color-sp li:nth-child(3)").click(function () {
+        $(this)
+            .parents(".wp-item-sp")
+            .find(".wp-item-sp-main:nth-child(3)")
+            .addClass("active");
     });
-    $(".list-color-sp li:nth-child(4)").click( function() {
-        $(this).parents(".wp-item-sp").find(".wp-item-sp-main:nth-child(4)").addClass('active');
+    $(".list-color-sp li:nth-child(4)").click(function () {
+        $(this)
+            .parents(".wp-item-sp")
+            .find(".wp-item-sp-main:nth-child(4)")
+            .addClass("active");
     });
-    $(".list-color-sp li:nth-child(5)").click( function() {
-        $(this).parents(".wp-item-sp").find(".wp-item-sp-main:nth-child(5)").addClass('active');
+    $(".list-color-sp li:nth-child(5)").click(function () {
+        $(this)
+            .parents(".wp-item-sp")
+            .find(".wp-item-sp-main:nth-child(5)")
+            .addClass("active");
     });
-    $(".list-color-sp li:nth-child(6)").click( function() {
-        $(this).parents(".wp-item-sp").find(".wp-item-sp-main:nth-child(6)").addClass('active');
+    $(".list-color-sp li:nth-child(6)").click(function () {
+        $(this)
+            .parents(".wp-item-sp")
+            .find(".wp-item-sp-main:nth-child(6)")
+            .addClass("active");
     });
-    $(".list-color-sp li:nth-child(7)").click( function() {
-        $(this).parents(".wp-item-sp").find(".wp-item-sp-main:nth-child(7)").addClass('active');
+    $(".list-color-sp li:nth-child(7)").click(function () {
+        $(this)
+            .parents(".wp-item-sp")
+            .find(".wp-item-sp-main:nth-child(7)")
+            .addClass("active");
     });
-    $(".list-color-sp li:nth-child(8)").click( function() {
-        $(this).parents(".wp-item-sp").find(".wp-item-sp-main:nth-child(8)").addClass('active');
+    $(".list-color-sp li:nth-child(8)").click(function () {
+        $(this)
+            .parents(".wp-item-sp")
+            .find(".wp-item-sp-main:nth-child(8)")
+            .addClass("active");
     });
-    $(".list-color-sp li:nth-child(9)").click( function() {
-        $(this).parents(".wp-item-sp").find(".wp-item-sp-main:nth-child(9)").addClass('active');
+    $(".list-color-sp li:nth-child(9)").click(function () {
+        $(this)
+            .parents(".wp-item-sp")
+            .find(".wp-item-sp-main:nth-child(9)")
+            .addClass("active");
     });
-    $(".list-color-sp li:nth-child(10)").click( function() {
-        $(this).parents(".wp-item-sp").find(".wp-item-sp-main:nth-child(10)").addClass('active');
+    $(".list-color-sp li:nth-child(10)").click(function () {
+        $(this)
+            .parents(".wp-item-sp")
+            .find(".wp-item-sp-main:nth-child(10)")
+            .addClass("active");
     });
 
     // function stick(id, el_class, offset_top, stick, unstick) {
@@ -286,70 +320,68 @@ jQuery(document).ready(function($) {
     // };    /*---------- Cố định menu dọc trang danh sách sản phẩm khi scroll ----------*/
     // stick(".pca-pl-r", "fixed-menu", 1, "", "");
 
-    $('.slide-sp-title').owlCarousel({
-        loop:true,
-        margin:0,
-        dots:false,
-        nav:true,
-        autoplay:false,
-        autoplayTimeout:3000,
-        autoplaySpeed:1200,
-        smartSpeed:1200,
-        responsive:{
-            0:{
-                items:3,
-                
+    $(".slide-sp-title").owlCarousel({
+        loop: true,
+        margin: 0,
+        dots: false,
+        nav: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        autoplaySpeed: 1200,
+        smartSpeed: 1200,
+        responsive: {
+            0: {
+                items: 3,
             },
-            600:{
-                items:5
+            600: {
+                items: 5,
             },
-            1000:{
-
-                items:5
-            }
-        }
-
+            1000: {
+                items: 5,
+            },
+        },
     });
 
-    $('.slide-sp').owlCarousel({
-        loop:true,
-        margin:0,
-        dots:false,
-        nav:true,
-        autoplay:false,
-        autoplayTimeout:3000,
-        autoplaySpeed:1200,
-        smartSpeed:1200,
-        responsive:{
-            0:{
-                items:1
+    $(".slide-sp").owlCarousel({
+        loop: true,
+        margin: 0,
+        dots: false,
+        nav: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        autoplaySpeed: 1200,
+        smartSpeed: 1200,
+        responsive: {
+            0: {
+                items: 1,
             },
-            600:{
-                items:1
+            600: {
+                items: 1,
             },
-            1000:{
-                items:1
-            }
-        }
+            1000: {
+                items: 1,
+            },
+        },
     });
-    $('.slide-sp-mobile').owlCarousel({
-        loop:true,margin:0,
-        dots:true,
-        nav:true,
-        autoplay:false,
-        autoplayTimeout:3000,
-        autoplaySpeed:1200,
-        smartSpeed:1200,
-        responsive:{
-            0:{
-                items:1
+    $(".slide-sp-mobile").owlCarousel({
+        loop: true,
+        margin: 0,
+        dots: true,
+        nav: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        autoplaySpeed: 1200,
+        smartSpeed: 1200,
+        responsive: {
+            0: {
+                items: 1,
             },
-            600:{
-                items:1
+            600: {
+                items: 1,
             },
-            1000:{
-                items:1
-            }
-        }
+            1000: {
+                items: 1,
+            },
+        },
     });
 });
