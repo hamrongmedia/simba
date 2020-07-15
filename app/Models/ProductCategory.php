a00482ae08d8b2ef375592242bada07cbc276a24
tranhoang-c5 <tranhoang750@gmail.com>
2020-07-15 14:08:24 +0700

    fix

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -7,13 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class ProductCategory extends Model
 {
     protected $table = "product_categories";
-    protected $fillable = ['name', 'description', 'slug', 'parent_category', 'meta_keyword', 'meta_title', 'meta_description', 'status', 'is_deleted', 'view'];
+    protected $fillable = ['name', 'description', 'slug', 'parent_category', 'meta_keyword', 'meta_title', 'meta_description', 'status', 'is_deleted'];
 
-    public function parentCategory(){
+    public function parentCategory()
+    {
         return $this->belongsTo('App\Models\ProductCategory', 'parent_category', 'id');
     }
 
-    public function subCategory(){
+    public function subCategory()
+    {
         return $this->hasMany('App\Models\ProductCategory', 'parent_category', 'id');
     }
 
@@ -23,7 +25,7 @@ class ProductCategory extends Model
      */
     public function products()
     {
-        return $this->belongsToMany(Product::class, 'product_to_categories','category_id','product_id');
+        return $this->belongsToMany(Product::class, 'product_to_categories', 'category_id', 'product_id');
     }
 
     protected static function boot()
