d8ee457fa4303b15489d074df9da0c2d14f9a30c
tranhoang-c5 <tranhoang750@gmail.com>
2020-07-24 15:56:22 +0700

    fix

--- resources/views/admin/pages/admin_manage/user_create.blade.php
+++ resources/views/admin/pages/admin_manage/user_create.blade.php
@@ -174,7 +174,7 @@ Tạo mới user
 @include('admin.component.ckeditor_js')
 
 <!-- Select2 -->
-<script src="{{asset('/admin/adminLTE/bower_components/select2/dist/js/select2.full.min.js')}}"></script>
+<script src="{{asset('/template/adminLTE/bower_components/select2/dist/js/select2.full.min.js')}}"></script>
 
 <script>
     //Initialize Select2 Elements
