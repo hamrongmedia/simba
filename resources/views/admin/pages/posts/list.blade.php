c43e66c58e82632e3b924071c78305b2408653e0
tranhoang-c5 <tranhoang750@gmail.com>
2020-07-25 10:21:07 +0700

    list post datatable

--- resources/views/admin/pages/posts/list.blade.php
+++ resources/views/admin/pages/posts/list.blade.php
@@ -2,34 +2,15 @@
 @section('title')
   Quản lý bài viết
 @endsection
-
+@section('css')
+{{-- Datatable --}}
+<link rel="stylesheet" href="{{ asset('template/AdminLTE/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}">
+@endsection
 @section('main')
 <div class="row">
   <div class="col-xs-12">
     <div class="box">
       <div class="box-body">
-        <div class="box-header with-border">
-          <div class="pull-right">
-              <div class="menu-right">
-                  <form action="{{route('admin.post.search')}}" id="button_search">
-                      <div onclick="searchAjax()" class="btn-group pull-right">
-                          <a class="btn btn-flat btn-primary" title="Refresh">
-                              <i class="fa  fa-search"></i>
-                          </a>
-                      </div>
-                      <div class="btn-group pull-right">
-                          <div class="form-group">
-                              <input type="text" id="search_input" name="query" class="form-control"
-                                  placeholder="Search Name, ID or Email" value="">
-                          </div>
-                      </div>
-                  </form>
-              </div>
-          </div>
-          <div class="pull-left">
-          </div>
-          <!-- /.box-tools -->
-      </div>
 
       <div class="box-header with-border">
           <div class="pull-right">
@@ -40,7 +21,7 @@
                   </a>
               </div>
           </div>
-          <div class="pull-left">
+          {{-- <div class="pull-left">
               <div class="menu-left">
                   <button type="button" class="btn btn-default grid-select-all"><i
                           class="fa fa-square-o"></i></button>
@@ -71,12 +52,12 @@
                       </a>
                   </div>
               </div>
-          </div>
+          </div> --}}
  
       </div>
       <section id="pjax-container" class="table-list">
-        @include('admin.pages.ajax_components.post_table')
-
+        {{-- @include('admin.pages.ajax_components.post_table') --}}
+        @include('admin.pages.posts.table')
       </section>
       </div>
       <!-- /.box-body -->
@@ -88,7 +69,34 @@
 @endsection
 
 @section('js')
+<script src="{{ asset('template/AdminLTE/bower_components/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{ asset('template/AdminLTE/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
+
 <script>
+
+$(function () {
+    $("#posts-table").dataTable({
+        processing: true,
+        serverSide: true,
+        responsive:true,
+        autoWidth:false,
+        scrollX: true,
+
+        ajax: "{{route('admin.post.list_post')}}",
+        columns: [
+            { "data": "id" },
+            { "data": "title" },
+            { "data": "slug" },
+            { "data": "categories" },
+            { "data" :"image"},
+            { "data" :"status"},
+            { "data" :"action"},
+
+        ]
+        });
+    });
+
+    
   var type = 'sort';
 
   function deleteAjax(id) {
